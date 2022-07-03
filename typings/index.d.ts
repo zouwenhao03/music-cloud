930bdcd68bfa90aabe4ebbae3889f09b348d66bd
zou <1094749143@qq.com>
2022-07-03 20:42:48 +0800

    6.30

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -4,8 +4,15 @@ interface IAppOption {
   globalData: {
     userInfo?: WechatMiniprogram.UserInfo,
     hasLogin:boolean,
-    musicId:string,
-    isMusicPlay:boolean
+    musicId:-1,
+    isMusicPlay:boolean,
+    userId: 0,
+    waitForPlaying: [],//等待播放歌单
+    history_songId: [],//历史歌单
+    songName: '',//歌名
+    backgroundAudioManager: {},
+    login_token: '',
+    navId: 2
   }
   userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
